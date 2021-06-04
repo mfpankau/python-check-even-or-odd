fe986d607f02fde1df1dcd452b50c004a836a11b
mfpankau <74994673+mfpankau@users.noreply.github.com>
2021-06-04 15:47:28 -0400

    Create checkEvenOrOdd.py

---
+++ checkEvenOrOdd.py
@@ -0,0 +1,24 @@
+import sys
+
+def makeFile(min, max):
+    f = open('temp.py', 'w')
+    f.write('def main(val):\n\tif(val == ' + str(min) + ') :\n')
+    if min % 2 == 0:
+        f.write('''\t\tprint('Even')\n''')
+    else:
+        f.write('''\t\tprint('Odd')\n''')
+    for i in range(min + 1, max + 1):
+        f.write('\telif(val == ' + str(i) + '):\n')
+        if i % 2 == 0:
+            f.write('''\t\tprint('Even')\n''')
+            continue
+        f.write('''\t\tprint('Odd')\n''')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage: ' + sys.argv[0] + ' <value to check>')
+    elif len(sys.argv) == 2:
+        makeFile(int(sys.argv[1]) - 10, int(sys.argv[1]) + 10)
+        import temp
+        temp.main(int(sys.argv[1]))
